f9f6f7a508784fd321a29e78e1e837d0274cea04
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2020-11-07 15:11:50 +0900

    Initial commit

---
+++ src/test/java/com/domino/t1/SqlSessionTsest.java
@@ -0,0 +1,19 @@
+package com.domino.t1;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SqlSessionTsest extends MyTestCase{
+	
+	@Autowired
+	private SqlSession sqlSession; 
+	
+	@Test
+	public void test() {
+		assertNotNull(sqlSession.getConnection());
+	}
+
+}
