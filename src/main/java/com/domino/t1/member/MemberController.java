6ce5c6787101634e75aecb945646d073fdbd27e0
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2020-11-09 15:38:33 +0900

    memberDAO, DTO, Service add

--- src/main/java/com/domino/t1/member/MemberController.java
+++ src/main/java/com/domino/t1/member/MemberController.java
@@ -1,5 +1,31 @@
 package com.domino.t1.member;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/member/**")
 public class MemberController {
+	
+	@Autowired
+	private MemberService memberService; 
+	
+	@GetMapping("memberList")
+	public ModelAndView getMemberList(MemberDTO memberDTO) throws Exception{
+		System.out.println("Member List");
+		ModelAndView mv = new ModelAndView();
+		
+		List<MemberDTO> ar = memberService.getMemberList(memberDTO);
+		
+		mv.addObject("list", ar);
+		mv.setViewName("member/memberList");
+		
+		return mv; 
+	}
 
 }
--- src/main/java/com/domino/t1/member/MemberService.java
+++ src/main/java/com/domino/t1/member/MemberService.java
@@ -1,5 +1,18 @@
 package com.domino.t1.member;
 
-public class MemberService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+		
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	public List<MemberDTO> getMemberList(MemberDTO memberDTO) throws Exception{
+		return memberDAO.getMemberList(memberDTO);
+		
+	}
 }
