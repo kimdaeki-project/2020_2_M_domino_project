d67c8e35a5e0eb27994fb95ccc7a069b7f877eb6
YeEun Kim <snakefeet42@gmail.com>
2020-11-11 12:28:46 +0900

    피자 리스트, 이미지 업데이트
    * list controller, service에 피자 리스트 관련 기능 업데이트 -> 프리미엄 피자 카테고리의 리스트만 우선 구현
    * 전 제품 이미지 추가
    * view>menu 하위 파일/폴더 구조 변경

--- src/main/java/com/domino/t1/menu/list/ListController.java
+++ src/main/java/com/domino/t1/menu/list/ListController.java
@@ -1,10 +1,14 @@
 package com.domino.t1.menu.list;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.domino.t1.item.pizza.PizzaDTO;
 
 @Controller
 @RequestMapping("/menu/list/**")
@@ -13,19 +17,37 @@ public class ListController {
 	@Autowired
 	private ListService listService;
 	
-	@GetMapping("pizza")
+	@GetMapping("pizzaList")
 	public ModelAndView getPizzaList() throws Exception{
 		ModelAndView mv = new ModelAndView();
 		
-		mv.setViewName("menu/list");
+		// get premium type PizzaDTO list
+		PizzaDTO dto= new PizzaDTO();
+		dto.setPizza_type("프리미엄");
+		List<PizzaDTO> arr = listService.getPizzaListByType(dto);
+		mv.addObject("premium", arr);
+		// get classic type PizzaDTO list
+		
+		mv.addObject("category", "pizza");
+		mv.setViewName("menu/list/pizzaList");
 		return mv;
 	}
 	
-	@GetMapping("sideMenu")
-	public ModelAndView getSideMenuList() throws Exception {
+	@GetMapping("sideDishList")
+	public ModelAndView getSideDishList() throws Exception {
 		ModelAndView mv = new ModelAndView();
 		
-		mv.setViewName("menu/list");
+		mv.addObject("menu", "sideDish");
+		mv.setViewName("menu/list/sideDishList");
+		return mv;
+	};
+	
+	@GetMapping("etcList")
+	public ModelAndView getEtcList() throws Exception {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.addObject("menu", "etc");
+		mv.setViewName("menu/list/sideDishList");
 		return mv;
 	}
 	
---
+++ src/main/webapp/WEB-INF/views/menu/detail/pizzaDetail.jsp
@@ -0,0 +1,32 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Insert title here</title>
+    <meta charset="utf-8">
+    <c:import url="../../template/bootstrap.jsp"></c:import>
+    <link href ="/t1/resources/css/common/default.css" rel="stylesheet">
+    <script src="/t1/resources/js/header.js"></script>
+    
+</head>
+<body>
+
+<c:import url="../../template/header.jsp"></c:import>
+<div>
+
+	<div>
+	
+	
+	</div>
+
+</div>
+
+	<script type="text/javascript">	
+	
+	</script>
+
+
+</body>
+</html>
