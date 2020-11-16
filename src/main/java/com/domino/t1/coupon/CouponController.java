c6ef2071840183fab143e56982788746b65b10b7
wogns5471 <66050816+wogns5471@users.noreply.github.com>
2020-11-16 17:50:17 +0900

    coupon-update

--- src/main/java/com/domino/t1/coupon/CouponController.java
+++ src/main/java/com/domino/t1/coupon/CouponController.java
@@ -1,4 +1,4 @@
-package com.domino.t1.order;
+package com.domino.t1.coupon;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +7,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/order/**")
-public class OrderController {
+public class CouponController {
 
-	@GetMapping("orderInfo")
+	@GetMapping("myCoupon")
 	public ModelAndView orderWrite() {
 		ModelAndView mv = new ModelAndView();
 				
-		mv.setViewName("order/orderInfo");
+		mv.setViewName("order/myCoupon");
 		
 		return mv;
-		
+
 	}
-	
-	
 	
 }
