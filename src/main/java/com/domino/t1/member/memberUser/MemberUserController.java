c892760931f65d14d4609ee9884557a97baf6e7a
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2020-11-17 17:17:23 +0900

    Update MemberUserController.java

--- src/main/java/com/domino/t1/member/memberUser/MemberUserController.java
+++ src/main/java/com/domino/t1/member/memberUser/MemberUserController.java
@@ -2,7 +2,7 @@ package com.domino.t1.member.memberUser;
 
 import java.util.List;
 
-import javax.jws.WebParam.Mode;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -10,8 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
