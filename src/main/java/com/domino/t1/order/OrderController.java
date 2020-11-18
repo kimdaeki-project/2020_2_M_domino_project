768f90968ff8e78c111a38c195a76dad943df4ca
wogns5471 <66050816+wogns5471@users.noreply.github.com>
2020-11-18 16:17:45 +0900

    update

--- src/main/java/com/domino/t1/order/OrderController.java
+++ src/main/java/com/domino/t1/order/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.domino.t1.member.memberUser.MemberUserDTO;
 import com.domino.t1.member.memberUser.MemberUserService;
 
 @Controller
@@ -15,7 +16,7 @@ public class OrderController {
 	
 
 	@GetMapping("orderInfo")
-	public ModelAndView orderWrite() {
+	public ModelAndView orderWrite(MemberUserDTO memberUserDTO) {
 		ModelAndView mv = new ModelAndView();
 		
 		mv.setViewName("order/orderInfo");
